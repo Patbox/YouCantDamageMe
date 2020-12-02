b69ab2216a3c66519f50d138f78dfd25c86aa3e4
Patbox <39821509+Patbox@users.noreply.github.com>
2020-12-02 12:08:01 +0100

    Add new gamerules

--- src/main/java/eu/pb4/youcantdamageme/YouCantDamageMe.java
+++ src/main/java/eu/pb4/youcantdamageme/YouCantDamageMe.java
@@ -7,6 +7,11 @@ import net.minecraft.world.GameRules;
 
 public class YouCantDamageMe implements ModInitializer {
     public static final GameRules.Key<GameRules.BooleanRule> DAMAGE_GAMERULE = GameRuleRegistry.register("playerDamage", GameRules.Category.PLAYER, GameRuleFactory.createBooleanRule(true));
+    public static final GameRules.Key<GameRules.IntRule> DAMAGE_SCALE_GAMERULE = GameRuleRegistry.register("playerDamageScale", GameRules.Category.PLAYER, GameRuleFactory.createIntRule(100, 1));
+
+    public static final GameRules.Key<GameRules.BooleanRule> MAGIC_DAMAGE_GAMERULE = GameRuleRegistry.register("magicDamage", GameRules.Category.PLAYER, GameRuleFactory.createBooleanRule(true));
+    public static final GameRules.Key<GameRules.BooleanRule> ENTITY_ATTACK_DAMAGE_GAMERULE = GameRuleRegistry.register("entityAttackDamage", GameRules.Category.PLAYER, GameRuleFactory.createBooleanRule(true));
+    public static final GameRules.Key<GameRules.BooleanRule> SELF_DAMAGE_GAMERULE = GameRuleRegistry.register("selfDamage", GameRules.Category.PLAYER, GameRuleFactory.createBooleanRule(true));
 
     public void onInitialize() { }
 }
--- src/main/java/eu/pb4/youcantdamageme/mixin/PlayerEntityMixin.java
+++ src/main/java/eu/pb4/youcantdamageme/mixin/PlayerEntityMixin.java
@@ -7,13 +7,18 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
+
+    @Shadow public abstract boolean damage(DamageSource source, float amount);
 
     @Inject(method = "applyDamage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;setAbsorptionAmount(F)V"), cancellable = true)
     private void cancelDamage(DamageSource source, float amount, CallbackInfo ci) {
@@ -21,6 +26,25 @@ public abstract class PlayerEntityMixin extends LivingEntity {
             ci.cancel();
         }
     }
+
+    @ModifyVariable(method = "applyDamage", at = @At("HEAD"), ordinal = 0)
+    private float scaleDamage(float y) {
+        return y * this.world.getGameRules().getInt(YouCantDamageMe.DAMAGE_SCALE_GAMERULE) / 100;
+    }
+
+    @Inject(method = "isInvulnerableTo", at = @At("TAIL"), cancellable = true)
+    private void otherSources(DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
+        if (damageSource.getAttacker() != null && !damageSource.getMagic() && damageSource.getAttacker() != this) {
+            cir.setReturnValue(!this.world.getGameRules().getBoolean(YouCantDamageMe.ENTITY_ATTACK_DAMAGE_GAMERULE));
+        } else if (damageSource.getAttacker() != null && !damageSource.getMagic() && damageSource.getAttacker() == this) {
+            cir.setReturnValue(!this.world.getGameRules().getBoolean(YouCantDamageMe.SELF_DAMAGE_GAMERULE));
+        }
+
+        else if (damageSource.getMagic()) cir.setReturnValue(!this.world.getGameRules().getBoolean(YouCantDamageMe.MAGIC_DAMAGE_GAMERULE));
+    }
+
+
+
 
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
